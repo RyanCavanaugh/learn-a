3b818652a43d54797d490f12e8e0cbed6fc4bc70
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2018-06-27 14:01:40 -0700

    Fix package.jsons

--- packages/pkg2/src/index.ts
+++ packages/pkg2/src/index.ts
@@ -1,5 +1,6 @@
 import * as p1 from '@ryancavanaugh/pkg1';
 
-export function fn3() {
+export function fn4() {
     p1.fn();
 }
+
---
+++ packages/pkg3/src/index.ts
@@ -0,0 +1,5 @@
+import * as p1 from "@ryancavanaugh/pkg1";
+import * as p2 from "@ryancavanaugh/pkg2";
+
+p1.fn();
+p2.fn4();
